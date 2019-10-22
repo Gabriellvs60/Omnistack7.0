8e3772d29d14af4eb6d9940874f6f95e9f2fbb93
Gabriel Coelho <gabriellvs60@gmail.com>
2019-10-22 20:50:54 -0300

    Routes
    Inicializando routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
+//react com o browser
 import ReactDOM from 'react-dom';
-
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ frontend/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import Feed from './pages/Feed';
+import New from './pages/New';
+
+function Routes(){
+    return(
+        //switch garante que apenas uma rota seja chamada
+        <Switch>
+            <Route path="/" exact component={Feed}/>
+            <Route path="/new" component={New}/>
+        </Switch>
+    );
+}
+
+export default Routes;
