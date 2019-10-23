2c9f98b3014a8c9ad1773647b2a3ea0d8b2b4c66
Gabriel Coelho <gabriellvs60@gmail.com>
2019-10-23 19:02:43 -0300

    Heade e content
    Criando a Header e Conteúdo da postagem

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,9 +1,56 @@
 import React, {Component} from 'react';
+import './Feed.css';
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
 
 class Feed extends Component{
     render(){
         return(
-            <h1>Feed</h1>
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Gabriel</span>
+                            <span>Pelotas, RS</span>
+                        </div>
+                        <img src={more} alt="Mais"/>
+                    </header>
+                    <img src="" alt=""/>
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt=""/>
+                            <img src={comment} alt=""/>
+                            <img src={send} alt=""/>
+                        </div>
+                        <strong>900 curtidas</strong>
+                        <p>Um post massa</p>
+                        <span>#Nice #Top</span>
+                    </footer>
+                </article>
+
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Gabriel</span>
+                            <span>Pelotas, RS</span>
+                        </div>
+                        <img src={more} alt="Mais"/>
+                    </header>
+                    <img src="" alt=""/>
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt=""/>
+                            <img src={comment} alt=""/>
+                            <img src={send} alt=""/>
+                        </div>
+                        <strong>900 curtidas</strong>
+                        <p>Um post massa</p>
+                        <span>#Nice #Top</span>
+                    </footer>
+                </article>
+            </section>
         );
     }
 }
