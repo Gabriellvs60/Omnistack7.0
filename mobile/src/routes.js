1a67c429b5e647775cb61f47351fa885dbfa1cbd
Gabriel Coelho <gabriellvs60@gmail.com>
2019-10-25 12:12:38 -0300

    Navegação
    Navegação entre paginas no react native

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,11 +1,25 @@
-import {createAppContainer, createStackNavigator} from 'react-navigarion';
+import React from 'react';
+import {createAppContainer} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import {Image} from 'react-native';
 
 import Feed from './pages/Feed';
 import New from './pages/New';
+
+import logo from './assets/logo.png';
 
 export default createAppContainer(
     createStackNavigator({
         Feed,
         New,
+    },{
+        //isso aplica a todas as telas
+        defaultNavigationOptions:{
+            headerTintColor: '#000',
+            headerTitle: <Image style={{marginHorizontal: 20}}source={logo} />
+            ,headerBackTitle: null,
+        },
+        mode: 'modal'
     })
 );
